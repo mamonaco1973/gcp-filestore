e03237004194b12aadbdd4061c8538b986ef06ce
Mike Monaco <mamonaco1973@gmail.com>
2026-02-23 19:45:00 -0500

    2026 tech refresh

--- validate.sh
+++ validate.sh
@@ -49,7 +49,7 @@ linux_ip="$(gcloud compute instances list \
 # ------------------------------------------------------------------------------
 echo ""
 echo "============================================================================"
-echo "NFS\Filestore Quick Start - Validation Output (GCP)"
+echo "NFS Filestore Quick Start - Validation Output (GCP)"
 echo "============================================================================"
 echo ""
 
